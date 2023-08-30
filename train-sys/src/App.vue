a1fe47ee5c7ad532ecf57f7debbff0293bea5f31
leoReiis <leonardoreis.ufsj@gmail.com>
2023-08-29 22:12:34 -0300

    feat: novo componente, footer

--- train-sys/src/App.vue
+++ train-sys/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <Header></Header>
   <RouterView />
+  <span class="footer">
+    <Footer></Footer>
+  </span>
 </template>
 
 <script>
@@ -8,10 +11,18 @@ import Login from "./components/Login.vue";
 import Signup from "./components/Signup.vue";
 import Dashboard from "./components/Dashboard.vue";
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
-  components: { Login, Signup, Dashboard, Header },
+  components: { Login, Signup, Dashboard, Header, Footer },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+}
+</style>
---
+++ train-sys/src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-layout>
+    <v-footer absolute class="text-center d-flex flex-column b-0">
+      <div>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4"
+          :icon="icon"
+          variant="text"
+        ></v-btn>
+
+        <v-btn
+          icon
+          variant="text"
+          class="mx-4"
+          href="https://www.linkedin.com/in/leoreiis/"
+          target="_blank"
+        >
+          <v-icon>mdi-linkedin</v-icon>
+        </v-btn>
+      </div>
+
+      <v-divider></v-divider>
+
+      <div>
+        {{ new Date().getFullYear() }} —
+        <strong> Saiya Gym</strong>
+      </div>
+    </v-footer>
+  </v-layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
